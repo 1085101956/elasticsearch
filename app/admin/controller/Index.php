f90a4617d5997c8206182c4de5820baff68ddaca
linzhiwei <1085101956@qq.com>
2023-01-28 14:41:20 +0800

    试试

---
+++ app/admin/controller/Index.php
@@ -0,0 +1,24 @@
+<?php
+namespace app\admin\controller;
+
+use app\BaseController;
+use app\Request;
+use Elasticsearch\ClientBuilder;
+
+class Index extends BaseController {
+
+    public function index (Request $request) {
+        $client = ClientBuilder::create()
+            ->setHosts(['localhost:9200'])
+            ->build();
+        $response = $client->info();
+        //响应格式化
+        // 响应格式化
+        $info = $response->asArray();
+
+        $data = [
+            'es_info' => $info,
+        ];
+        echo json($data);
+    }
+}
---
+++ app/admin/route/app.php
@@ -0,0 +1,24 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006~2018 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+use think\facade\Route;
+
+
+Route::get('niha','index/niha');
+
+Route::get("api/seach","index/index");
+
+
+//链接并绑定socket
+Route::get('api/testSocket','index/testSocket');
+//个人发送消息
+Route::get('api/sendPersonalSocket','index/sendPersonalSocket');
+//发送群组消息
+Route::get('api/sendGroupSocket','index/sendGroupSocket');
